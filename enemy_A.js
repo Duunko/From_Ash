0ec86e0ab2e43762e290b276916b3fca3042e4a4
bion719 <djchambe@ucsc.edu>
2016-05-11 12:16:31 -0700

    improved mapx/y to canvasx/y inegration

---
+++ enemy_A.js
@@ -0,0 +1,51 @@
+/**
+ * @author Bion719
+ */
+
+/* Basic character object
+ * This is the object for the basic enemy type. Contains a sprite,
+ * fire point value, hit point value, directional data (for bookeeping),
+ * x and y coordinates, and an update and draw method. 
+ * 
+ * Also contains an attack() and special() method that are unimpelemented.
+ * 
+ */
+ 
+function enemy_a(x, y){
+	this.sprite = new Image();
+	this.sprite.src = 'http://people.ucsc.edu/~djchambe/cm120/sun.png';
+	this.sprite.width = 40;
+	this.sprite.height = 40;
+	
+	this.move_direc = 'south';
+	this.look_direc = 'south';
+	
+	this.canvasX = x;
+	this.canvasY = y;
+	this.mapX = x; //-------------------Create formula for canvasX to mapX
+	this.mapY = y;
+	
+	this.speed = 5;
+	
+	this.update = function(){
+		//if main character is alive
+		moveTowards(MC);
+	}
+	
+	this.draw = function(){
+		
+	}
+	
+	this.moveTowards = function(target){
+		target.x;
+		target.y;
+	}
+	
+	this.attack = function(){
+		
+	}
+	
+	this.destroy = function(){
+		
+	}
+}
